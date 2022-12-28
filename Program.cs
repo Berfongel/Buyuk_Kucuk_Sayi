5071eca19a81a7bc7b6b8c124623c1614979a6f7
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:01:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+double sayi1;
+double sayi2;
+
+Console.WriteLine("Birinci sayıyı giriniz :");
+sayi1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("İkinci sayıyı giriniz :");
+sayi2 = Convert.ToDouble(Console.ReadLine());
+
+if (sayi1>sayi2) 
+{
+    Console.WriteLine("Büyük sayı : " + sayi1);
+    Console.WriteLine("Küçük sayı : " + sayi2);
+}
+else if (sayi1 == sayi2)
+{
+    Console.WriteLine("Girilen sayılar eşittir. ");
+}
+else 
+{
+    Console.WriteLine("Büyük sayı : " + sayi2);
+    Console.WriteLine("Küçük sayı : " + sayi1);
+}

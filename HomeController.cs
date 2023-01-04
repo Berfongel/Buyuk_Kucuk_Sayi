35a02ba3ec417d56d4afeb842804f45e0eb26af9
Berfongel <121492984+Berfongel@users.noreply.github.com>
2023-01-04 16:51:42 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SayiKücükBüyükWebApplication.Models;
+using System.Diagnostics;
+
+namespace SayiKücükBüyükWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(SayiKücükBüyük sayiKücükBüyük)
+        {
+            var firstnumber = sayiKücükBüyük.Number1;
+            var secondnumber = sayiKücükBüyük.Number2;
+            var firstmessage = string.Empty;
+            var secondmessage = string.Empty;
+            if (firstnumber > secondnumber)
+            {
+                firstmessage = firstnumber + " büyük sayı.";
+                secondmessage = secondnumber + " küçük sayı";
+
+            }
+
+            else if (firstnumber == secondnumber)
+            {
+                firstmessage = "Girilen sayılar birbirine eşittir.";
+            }
+            else
+            {
+                firstmessage = firstnumber + " küçük sayı";
+                secondmessage = secondnumber + " büyük sayı";
+            }
+
+            ViewBag.FirstMessage = firstmessage;
+            ViewBag.SecondMessage = secondmessage;
+            return View("sayiKücükBüyük");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SayiKücükBüyük.cs
@@ -0,0 +1,9 @@
+﻿namespace SayiKücükBüyükWebApplication.Models
+{
+    public class SayiKücükBüyük
+    {
+
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+    }
+}
